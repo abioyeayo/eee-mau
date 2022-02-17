b886f5de7b416a1e4837577ff0b8961cda851f2f
Ayo <aoa2g15@soton.ac.uk>
2022-02-17 11:06:11 +0100

    modified author meta data

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="author" content="setscentral">
+    <meta name="author" content="setscentral ltd. team - Moses, Chime, & Ayo">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles/style.css">
